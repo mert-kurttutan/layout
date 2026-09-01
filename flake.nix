3923b5ea40a08c6465954bce46bf49c285646d26
mert-kurttutan <kurttutan.mert@gmail.com>
2026-09-01 19:36:59 +0300

    Add Nix development flake

---
+++ flake.nix
@@ -0,0 +1,49 @@
+{
+  description = "Development shell for layout-rs";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+
+      forAllSystems =
+        f:
+        nixpkgs.lib.genAttrs systems (
+          system:
+          f {
+            pkgs = import nixpkgs { inherit system; };
+          }
+        );
+    in
+    {
+      devShells = forAllSystems (
+        { pkgs }:
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              cargo
+              clippy
+              graphviz
+              rustc
+              rustfmt
+            ];
+
+            shellHook = ''
+              echo "layout-rs dev shell"
+              echo "Rust: $(rustc --version)"
+              echo "Graphviz: $(dot -V 2>&1)"
+            '';
+          };
+        }
+      );
+    };
+}
