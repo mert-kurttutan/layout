c7e77cbc8fcc5de4bbdf8cdc19f26320488ca8aa
mert-kurttutan <kurttutan.mert@gmail.com>
2026-09-10 12:25:48 +0300

    Use NixOS zstd channel tarball

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Development shell for layout-rs";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "https://channels.nixos.org/nixos-unstable/nixexprs.tar.zst";
   };
 
   outputs =
